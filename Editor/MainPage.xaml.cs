480e7d34d8a0414f8ca06bf50ee81a7bb72c0065
Can <can.talat.yakan@gmail.com>
2021-05-20 10:38:29 +0200

    Renamed Control to GameMode and Added Enum EGameMode to it and added .Control to namespace

--- Editor/MainPage.xaml.cs
+++ Editor/MainPage.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Editor.Assets.Control;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,8 +23,12 @@ namespace Editor
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        GameMode m_gameMode;
+
         public MainPage()
         {
+            m_gameMode = new GameMode();
+
             this.InitializeComponent();
         }
 
