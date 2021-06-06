5cbd56e041c809a5d27c003f1c88b2f1aa181d11
Can <can.talat.yakan@gmail.com>
2021-06-06 23:59:15 +0200

    Added Object holding all Infos about Mesh and ID

---
+++ Editor/Assets/Engine/Obj.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage.Streams;
+
+namespace Editor.Assets.Engine
+{    
+    public class Obj
+    {
+        public Guid Id;
+
+        public IBuffer Data;
+        public List<CVertex> Vertices;
+        public uint VertexStride;
+        public List<ushort> Indices;
+        public uint IndexStride;
+
+        public Obj()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
