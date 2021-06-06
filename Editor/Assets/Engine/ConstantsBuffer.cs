f4d28be410f628f471daf6908f72db4663a960cc
Can <can.talat.yakan@gmail.com>
2021-06-06 16:48:16 +0200

    Added Utilities for DirectX

---
+++ Editor/Assets/Engine/ConstantsBuffer.cs
@@ -0,0 +1,15 @@
+﻿namespace Editor.Assets.Engine
+{
+  using SharpDX.Mathematics.Interop;
+
+  struct PerModelConstantsBuffer
+  {
+    public RawMatrix modelToWorld;
+  }
+  struct ViewConstantsBuffer
+  {
+    public RawMatrix view;
+    public RawMatrix projection;
+  }
+}
+
---
+++ Editor/Assets/Engine/MeshBufferInfo.cs
@@ -0,0 +1,17 @@
+﻿namespace Editor.Assets.Engine
+{
+  using Windows.Graphics.DirectX;
+  using D3D11 = SharpDX.Direct3D11;
+
+  class MeshBufferInfo
+  {
+    public D3D11.Buffer vertexBuffer;
+    public D3D11.Buffer indexBuffer;
+    public D3D11.Buffer constantsBuffer;
+    public DirectXPixelFormat indexFormat;
+    public int vertexCount;
+    public int indexCount;
+    public uint vertexStride;
+  }
+}
+
