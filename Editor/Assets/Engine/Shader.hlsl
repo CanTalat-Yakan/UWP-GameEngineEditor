8e2e7e91854b63991b8c9884b702d67855da01f5
Can <can.talat.yakan@gmail.com>
2021-06-06 16:48:56 +0200

    Added Shader with Pixel and Vertex Functions only supporting position in layout

---
+++ Editor/Assets/Engine/Shader.hlsl
@@ -0,0 +1,26 @@
+﻿cbuffer ConstantBuffer : register(b0)
+{
+	matrix view;
+	matrix projection;
+};
+
+cbuffer PerModelConstantBuffer : register(b1)
+{
+	matrix world;
+};
+
+float4 VS(min16float3 inPos : POSITION) : SV_POSITION
+{
+	float4 pos = float4(inPos, 1.0f);
+
+	pos = mul(pos, world);
+	pos = mul(pos, view);
+	pos = mul(pos, projection);
+
+	return(pos);
+}
+
+float4 PS(float4 notUsed : SV_POSITION) : SV_TARGET
+{
+    return float4(0.2f, 0.2f, 0.2f, 1.0f);
+}
