b2685c31414197b016c0fdda24c564015d022168
Can <can.talat.yakan@gmail.com>
2021-06-11 16:04:06 +0200

    Removed Restart Button and replaced PreAlpha with centered Early Access

--- Editor/Views/View_Main.xaml.cs
+++ Editor/Views/View_Main.xaml.cs
@@ -51,7 +51,6 @@ namespace Editor
                 x_AppBarToggleButton_Status_Play,
                 x_AppBarToggleButton_Status_Pause,
                 x_AppBarButton_Status_Forward,
-                x_AppBarButton_Status_Repeat,
                 x_AppBarButton_Status_Kill,
                 x_TextBlock_Status_Content,
                 m_Layout.m_ViewOutput.m_control);
@@ -60,7 +59,6 @@ namespace Editor
         private void AppBarToggleButton_Status_Play_Click(object sender, RoutedEventArgs e) { m_GameMode.Play(); }
         private void AppBarToggleButton_Status_Pause_Click(object sender, RoutedEventArgs e) { m_GameMode.Pause(); }
         private void AppBarButton_Status_Forward_Click(object sender, RoutedEventArgs e) { m_GameMode.Forward(); }
-        private void AppBarButton_Status_Repeat_Click(object sender, RoutedEventArgs e) { m_GameMode.Repeat(); }
         private void AppBarButton_Status_Kill_Click(object sender, RoutedEventArgs e) { m_GameMode.Kill(); }
         private void AppBarToggleButton_Status_Light(object sender, RoutedEventArgs e) { RequestedTheme = RequestedTheme == ElementTheme.Light ? ElementTheme.Dark : RequestedTheme = ElementTheme.Light; }
 
