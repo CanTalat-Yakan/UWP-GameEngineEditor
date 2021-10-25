842bde5cf279210d8687355aaaf7ad5632d7b191
CanTalat-Yakan <can.talat.yakan@gmail.com>
2021-10-26 00:03:29 +0200

    Commented out Imgui ToFix

--- Editor/Assets/Engine/Editor/Engine_ImGui.cs
+++ Editor/Assets/Engine/Editor/Engine_ImGui.cs
@@ -1,4 +1,6 @@
-﻿using Editor.Assets.Engine.Utilities;
+﻿using Editor.Assets.Engine.Components;
+using Editor.Assets.Engine.Data;
+using Editor.Assets.Engine.Utilities;
 using ImGuiNET;
 using System;
 using System.Numerics;
@@ -90,108 +92,104 @@ namespace Editor.Assets.Engine.Editor
             RenderCommandLists(drawData);
         }
 
-
         private unsafe void UpdateBuffers(ImDrawDataPtr drawData)
         {
             if (drawData.TotalVtxCount == 0)
-            {
                 return;
-            }
+            //// Expand buffers if we need more room
+            //if (drawData.TotalVtxCount > _vertexBufferSize)
+            //{
+            //    _vertexBuffer?.Dispose();
 
-            // Expand buffers if we need more room
-            if (drawData.TotalVtxCount > _vertexBufferSize)
-            {
-                _vertexBuffer?.Dispose();
+            //    _vertexBufferSize = (int)(drawData.TotalVtxCount * 1.5f);
+            //    _vertexBuffer = new VertexBuffer(_graphicsDevice, DrawVertDeclaration.Declaration, _vertexBufferSize, BufferUsage.None);
+            //    _vertexData = new byte[_vertexBufferSize * DrawVertDeclaration.Size];
+            //}
 
-                _vertexBufferSize = (int)(drawData.TotalVtxCount * 1.5f);
-                _vertexBuffer = new VertexBuffer(_graphicsDevice, DrawVertDeclaration.Declaration, _vertexBufferSize, BufferUsage.None);
-                _vertexData = new byte[_vertexBufferSize * DrawVertDeclaration.Size];
-            }
+            //if (drawData.TotalIdxCount > _indexBufferSize)
+            //{
+            //    _indexBuffer?.Dispose();
 
-            if (drawData.TotalIdxCount > _indexBufferSize)
-            {
-                _indexBuffer?.Dispose();
+            //    _indexBufferSize = (int)(drawData.TotalIdxCount * 1.5f);
+            //    _indexBuffer = new IndexBuffer(_graphicsDevice, IndexElementSize.SixteenBits, _indexBufferSize, BufferUsage.None);
+            //    _indexData = new byte[_indexBufferSize * sizeof(ushort)];
+            //}
 
-                _indexBufferSize = (int)(drawData.TotalIdxCount * 1.5f);
-                _indexBuffer = new IndexBuffer(_graphicsDevice, IndexElementSize.SixteenBits, _indexBufferSize, BufferUsage.None);
-                _indexData = new byte[_indexBufferSize * sizeof(ushort)];
-            }
+            //// Copy ImGui's vertices and indices to a set of managed byte arrays
+            //int vtxOffset = 0;
+            //int idxOffset = 0;
 
-            // Copy ImGui's vertices and indices to a set of managed byte arrays
-            int vtxOffset = 0;
-            int idxOffset = 0;
+            //for (int n = 0; n < drawData.CmdListsCount; n++)
+            //{
+            //    ImDrawListPtr cmdList = drawData.CmdListsRange[n];
 
-            for (int n = 0; n < drawData.CmdListsCount; n++)
-            {
-                ImDrawListPtr cmdList = drawData.CmdListsRange[n];
+            //    fixed (void* vtxDstPtr = &_vertexData[vtxOffset * DrawVertDeclaration.Size])
+            //    fixed (void* idxDstPtr = &_indexData[idxOffset * sizeof(ushort)])
+            //    {
+            //        Buffer.MemoryCopy((void*)cmdList.VtxBuffer.Data, vtxDstPtr, _vertexData.Length, cmdList.VtxBuffer.Size * DrawVertDeclaration.Size);
+            //        Buffer.MemoryCopy((void*)cmdList.IdxBuffer.Data, idxDstPtr, _indexData.Length, cmdList.IdxBuffer.Size * sizeof(ushort));
+            //    }
 
-                fixed (void* vtxDstPtr = &_vertexData[vtxOffset * DrawVertDeclaration.Size])
-                fixed (void* idxDstPtr = &_indexData[idxOffset * sizeof(ushort)])
-                {
-                    Buffer.MemoryCopy((void*)cmdList.VtxBuffer.Data, vtxDstPtr, _vertexData.Length, cmdList.VtxBuffer.Size * DrawVertDeclaration.Size);
-                    Buffer.MemoryCopy((void*)cmdList.IdxBuffer.Data, idxDstPtr, _indexData.Length, cmdList.IdxBuffer.Size * sizeof(ushort));
-                }
+            //    vtxOffset += cmdList.VtxBuffer.Size;
+            //    idxOffset += cmdList.IdxBuffer.Size;
+            //}
 
-                vtxOffset += cmdList.VtxBuffer.Size;
-                idxOffset += cmdList.IdxBuffer.Size;
-            }
-
-            // Copy the managed byte arrays to the gpu vertex- and index buffers
-            _vertexBuffer.SetData(_vertexData, 0, drawData.TotalVtxCount * DrawVertDeclaration.Size);
-            _indexBuffer.SetData(_indexData, 0, drawData.TotalIdxCount * sizeof(ushort));
+            //// Copy the managed byte arrays to the gpu vertex- and index buffers
+            //_vertexBuffer.SetData(_vertexData, 0, drawData.TotalVtxCount * DrawVertDeclaration.Size);
+            //_indexBuffer.SetData(_indexData, 0, drawData.TotalIdxCount * sizeof(ushort));
         }
 
         private unsafe void RenderCommandLists(ImDrawDataPtr drawData)
         {
-            _graphicsDevice.SetVertexBuffer(_vertexBuffer);
-            _graphicsDevice.Indices = _indexBuffer;
+//            _graphicsDevice.SetVertexBuffer(_vertexBuffer);
+//            _graphicsDevice.Indices = _indexBuffer;
 
-            int vtxOffset = 0;
-            int idxOffset = 0;
+//            int vtxOffset = 0;
+//            int idxOffset = 0;
 
-            for (int n = 0; n < drawData.CmdListsCount; n++)
-            {
-                ImDrawListPtr cmdList = drawData.CmdListsRange[n];
+//            for (int n = 0; n < drawData.CmdListsCount; n++)
+//            {
+//                ImDrawListPtr cmdList = drawData.CmdListsRange[n];
 
-                for (int cmdi = 0; cmdi < cmdList.CmdBuffer.Size; cmdi++)
-                {
-                    ImDrawCmdPtr drawCmd = cmdList.CmdBuffer[cmdi];
+//                for (int cmdi = 0; cmdi < cmdList.CmdBuffer.Size; cmdi++)
+//                {
+//                    ImDrawCmdPtr drawCmd = cmdList.CmdBuffer[cmdi];
 
-                    if (!_loadedTextures.ContainsKey(drawCmd.TextureId))
-                    {
-                        throw new InvalidOperationException($"Could not find a texture with id '{drawCmd.TextureId}', please check your bindings");
-                    }
+//                    if (!_loadedTextures.ContainsKey(drawCmd.TextureId))
+//                    {
+//                        throw new InvalidOperationException($"Could not find a texture with id '{drawCmd.TextureId}', please check your bindings");
+//                    }
 
-                    _graphicsDevice.ScissorRectangle = new Rectangle(
-                        (int)drawCmd.ClipRect.X,
-                        (int)drawCmd.ClipRect.Y,
-                        (int)(drawCmd.ClipRect.Z - drawCmd.ClipRect.X),
-                        (int)(drawCmd.ClipRect.W - drawCmd.ClipRect.Y)
-                    );
+//                    _graphicsDevice.ScissorRectangle = new Rectangle(
+//                        (int)drawCmd.ClipRect.X,
+//                        (int)drawCmd.ClipRect.Y,
+//                        (int)(drawCmd.ClipRect.Z - drawCmd.ClipRect.X),
+//                        (int)(drawCmd.ClipRect.W - drawCmd.ClipRect.Y)
+//                    );
 
-                    var effect = UpdateEffect(_loadedTextures[drawCmd.TextureId]);
+//                    var effect = UpdateEffect(_loadedTextures[drawCmd.TextureId]);
 
-                    foreach (var pass in effect.CurrentTechnique.Passes)
-                    {
-                        pass.Apply();
+//                    foreach (var pass in effect.CurrentTechnique.Passes)
+//                    {
+//                        pass.Apply();
 
-#pragma warning disable CS0618 // // FNA does not expose an alternative method.
-                        _graphicsDevice.DrawIndexedPrimitives(
-                            primitiveType: PrimitiveType.TriangleList,
-                            baseVertex: vtxOffset,
-                            minVertexIndex: 0,
-                            numVertices: cmdList.VtxBuffer.Size,
-                            startIndex: idxOffset,
-                            primitiveCount: (int)drawCmd.ElemCount / 3
-                        );
-#pragma warning restore CS0618
-                    }
+//#pragma warning disable CS0618 // // FNA does not expose an alternative method.
+//                        _graphicsDevice.DrawIndexedPrimitives(
+//                            primitiveType: PrimitiveType.TriangleList,
+//                            baseVertex: vtxOffset,
+//                            minVertexIndex: 0,
+//                            numVertices: cmdList.VtxBuffer.Size,
+//                            startIndex: idxOffset,
+//                            primitiveCount: (int)drawCmd.ElemCount / 3
+//                        );
+//#pragma warning restore CS0618
+//                    }
 
-                    idxOffset += (int)drawCmd.ElemCount;
-                }
+//                    idxOffset += (int)drawCmd.ElemCount;
+//                }
 
-                vtxOffset += cmdList.VtxBuffer.Size;
-            }
+//                vtxOffset += cmdList.VtxBuffer.Size;
+//            }
         }
 
     }
