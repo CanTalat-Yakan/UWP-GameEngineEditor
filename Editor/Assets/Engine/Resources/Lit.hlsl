41be4d3ef1fd7203ff05911ea84d5d1f299084ce
CanTalat-Yakan <can.talat.yakan@gmail.com>
2021-11-09 15:34:03 +0100

    Cleaned up shader

--- Editor/Assets/Engine/Resources/Lit.hlsl
+++ Editor/Assets/Engine/Resources/Lit.hlsl
@@ -47,8 +47,6 @@ float4 PS(VS_OUTPUT i) : SV_TARGET
     float4 col = ObjTexture.Sample(ObjSamplerState, i.uv);
     float3 diffuse = dot(normalize(i.normal), -normalize(float3(0, -1, 0)));
 
-    //float4 colReflect = ObjSkyBox.SampleLevel(ObjSamplerState, ReflectUV(reflect(-i.worldPos - i.camPos, normal)), params.roughness * 10);
-    //colReflect = CalculateReflection(colReflect, normal, i.worldPos - i.camPos);
     diffuse = max(diffuse, float3(0.255, 0.295, 0.3255));
 
     return col * float4(diffuse, 1);
